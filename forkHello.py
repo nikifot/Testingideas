2054cf9598732b0b9eb36c63d489b32bc67c9bfd
nikifot <33758698+nikifot@users.noreply.github.com>
2018-01-24 10:01:13 +0000

    update

--- forkHello.py
+++ forkHello.py
@@ -1,10 +1,9 @@
 import multiprocessing
 import os
 
-for i in range(0, 5):
-    tid = os.fork()
-    if tid == 0:
-        print('Hello from child')
-        tid.__e
-    else:
-        print('this is parent!')
+# for i in range(0, 5):
+tid = os.fork()
+if tid == 0:
+    print('Hello from child')
+else:
+    print('this is parent!')
