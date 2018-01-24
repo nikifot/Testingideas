ff5cac4636169961ae6f2d32b6e58c81c9370120
nikifot <33758698+nikifot@users.noreply.github.com>
2018-01-24 09:55:35 +0000

    initial commit

---
+++ forkHello.py
@@ -0,0 +1,10 @@
+import multiprocessing
+import os
+
+for i in range(0, 5):
+    tid = os.fork()
+    if tid == 0:
+        print('Hello from child')
+        tid.__e
+    else:
+        print('this is parent!')
