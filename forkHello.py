b6861fb3c7d1de8e705b2b8c4e73e291ac662e2e
nikifot <33758698+nikifot@users.noreply.github.com>
2018-01-24 10:56:40 +0000

    3.0

--- forkHello.py
+++ forkHello.py
@@ -1,13 +1,19 @@
 import multiprocessing
 import os
 
+def factorial(number):
+    for i in range(number-1, 0, -1):
+        number = number * i
+    return number
+
 # for i in range(0, 5):
 print('this is parent!')
 
-for i in range(5):
+for i in range(1, 10):
     tid = os.fork()
     if tid:
         print('Hello from child: %d' % tid)
+        print('the factorial of {0} is {1}'.format({i, factorial(i)}))
     else:
         print('closing')
         os._exit(0)
